8f11117f31d53abd9638aa182ce91a96c7c2cb1c
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-05-12 02:11:32 +0600

    handle submit button

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value; 
+        console.log(email, password);
+        
+    }
+
+    const handleEmailChange = (event) => {
+        // console.log(event.target.value);
+        setEmail(event.target.value);
+    } 
+
+    const handlePasswordBlur = (event) => {
+        // console.log(event.target.value);
+    }
+
     return (
         <div>
-           <h1>Register</h1> 
+           <h1> Please Register</h1> 
+           <form onSubmit={handleSubmit}>
+            <input onChange={handleEmailChange} type='email' name='email' id='email' placeholder='your email'></input>
+            <br />
+            <input onBlur={handlePasswordBlur} type='password' name='password' id='password' placeholder='your password'></input>
+            <br />
+            <input type='submit' value='Register'></input>
+           </form>
         </div>
     );
 };
